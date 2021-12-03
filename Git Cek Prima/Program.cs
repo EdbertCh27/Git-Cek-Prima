1544a2e050582d21750304d86461c949603fe363
EdbertCh27 <edbertch27@gmail.com>
2021-12-03 11:31:39 +0800

    Coding Cek Prima atau Bukan Prima (SELESAI)

--- Git Cek Prima/Program.cs
+++ Git Cek Prima/Program.cs
@@ -6,7 +6,41 @@ namespace Git_Cek_Prima
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+			while(true)
+            {
+				Console.WriteLine("Cek Prima = ");
+				int angka = Convert.ToInt32(Console.ReadLine());
+				int faktor = 0;
+				for (int i = 1; i <= angka; i++)
+				{
+					if (angka % i == 0)
+					{
+						faktor++;
+					}
+				}
+				if (faktor == 2)
+				{
+					Console.WriteLine("Merupakan Bilangan Prima");
+				}
+				else
+				{
+					Console.WriteLine("Bukan Bilangan Prima");
+				}
+
+				Console.Write("Cek Prima Lagi? (y/n) ");
+				string input = Console.ReadLine();
+				while (input != "y" && input != "n")
+				{
+					Console.Write("Ulangi ? (y/n) ");
+					input = Console.ReadLine();
+				}
+
+				if (input == "n")
+				{
+					break;
+				}
+			}
+			
+		}
     }
 }
